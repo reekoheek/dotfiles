6f0080fb6b2c01829232d24edab8876f290879a6
Ganesha <reekoheek@gmail.com>
2026-01-02 16:14:13 +0700

    fix: screenshot in labwc

--- scripts/scripts/watch-clipboard.sh
+++ scripts/scripts/watch-clipboard.sh
@@ -7,5 +7,5 @@ wl-paste --watch xclip -selection clipboard > /dev/null 2>&1 &
 
 # x11 -> wayland
 while clipnotify; do
-	xclip -selection clipboard -o | wl-copy
+		xclip -selection clipboard -o | wl-copy
 done > /dev/null 2>&1 &
