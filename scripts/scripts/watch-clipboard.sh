98151ea1c9fb6a10efe2bddd012211bbae3e9e06
Ganesha <reekoheek@gmail.com>
2026-01-02 15:22:28 +0700

    fix: labwc env

---
+++ scripts/scripts/watch-clipboard.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# clipboard sync wayland <-> x11 (untuk wine/xwayland apps)
+
+# wayland -> x11
+wl-paste --watch xclip -selection clipboard > /dev/null 2>&1 &
+
+# x11 -> wayland
+while clipnotify; do
+	xclip -selection clipboard -o | wl-copy
+done > /dev/null 2>&1 &
