4f8459f24bafe7ca0ac679a1acc4b82661bf59fc
Ganesha <reekoheek@gmail.com>
2026-02-02 19:46:27 +0700

    refactor: migrate yambar x11 config to use systower and remove obsolete scripts

--- scripts/scripts/disk-usage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-percent=$(df / | awk 'NR==2 {gsub(/%/,""); print $5}')
-echo "percent|int|$percent"
-echo
--- scripts/scripts/mem-usage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Total memory usage (RAM + all swap) in GB
-used=$(free -b | awk '/^Mem:/ {ram=$3} /^Swap:/ {swap=$3} END {printf "%.0f", (ram+swap)/1024/1024/1024}')
-
-echo "used|int|$used"
-echo
