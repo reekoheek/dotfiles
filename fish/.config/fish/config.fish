1799c35e6c13e614e1100140af25e8741632d69d
Ganesha <reekoheek@gmail.com>
2026-07-17 19:58:24 +0700

    feat(fish): add pnpm PATH and source secrets from external file

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,8 +2,20 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 set -Ux SUDO_EDITOR nvim
 
+# load secrets
+if test -f ~/secrets/config.fish
+  source ~/secrets/config.fish
+end
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
 #    # smth smth
 #end
+
+# pnpm
+set -gx PNPM_HOME "/home/reekoheek/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
